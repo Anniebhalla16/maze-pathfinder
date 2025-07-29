7a15c6987d6b69a48079daccfea204666637a5fc
anniebhalla16 <anniebhalla16@gmail.com>
2025-07-29 16:38:09 +0200

    fix: bash script

--- maze.sh
+++ maze.sh
@@ -2,11 +2,13 @@
 
 echo "🚀 Starting frontend (web-ui)"
 cd web-ui
+npm i &
 npm run dev &
 FRONTEND_PID=$!
 
 echo "🧠 Starting backend (web-api)"
 cd ../web-api
+npm i &
 npx ts-node server.ts
 
 kill $FRONTEND_PID   
